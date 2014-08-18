35cce393b89ef6628a51f1dad1fed308648e4f4e
Ravee Khandagale <Ravee@includes-iMac.local>
2014-08-18 14:39:58 +0530

    Initial Commit

---
+++ RaveeRushi/SecondViewController.h
@@ -0,0 +1,13 @@
+//
+//  SecondViewController.h
+//  RaveeRushi
+//
+//  Created by Ravee Khandagale on 18/08/14.
+//  Copyright (c) 2014 ___FULLUSERNAME___. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SecondViewController : UIViewController
+
+@end
